805268d29810661a7b9fdf4e033d24587d2903ab
Zainal21 <Zainalarifin080718@gmail.com>
2026-08-04 16:57:13 +0700

    feat: update @astrojs/mdx to version 7.0.0 and add content configuration for blog collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
 
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: 'src/content/blog/**/*.{md,mdx}' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
